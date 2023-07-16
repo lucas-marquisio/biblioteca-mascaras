ca72c55a45379e787ea7c64c5585ef7292d7a2b5
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2023-07-16 16:34:48 -0300

    add: mask parseNumber

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export const sum = (x: number, y: number): number => x + y
+export const parseNumber = (number: number): string => {
+  const suffixes: { [divisor: number]: string } = {
+    1: '',
+    1000: 'k',
+    1000000: 'm',
+    1000000000: 'b',
+    1000000000000: 't'
+  }
+
+  const divisorKeys = Object.keys(suffixes)
+    .map(key => parseInt(key))
+    .sort((a, b) => b - a) 
+
+  for (let divisor of divisorKeys) {
+    if (number >= divisor) {
+      const quotient = number / divisor
+      let result = quotient.toString()
+      if (result.includes('.')) {
+        result = result.replace(/(\.[0-9]*[1-9])0+$/, '$1')
+      }
+      return result + suffixes[divisor]
+    }
+  }
+
+  return number.toString()
+}
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+import * as lib from './index'
+
+console.log(lib.parseNumber(1003))
