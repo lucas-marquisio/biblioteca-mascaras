886318d8e56bd0ea0fcb7ab9479abe3a7491cab5
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2023-07-16 19:52:15 -0300

    fix: types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,9 @@
-// lib.d.ts
+declare const maskNumber: (input: number) => string;
+declare const maskDate: (input: string) => string;
+declare const maskCurrency: (input: string | number) => string;
+declare const maskCpf: (input: string | number) => string;
+declare const maskCnpj: (input: string | number) => string;
+declare const maskPhone: (input: string | number) => string;
+declare const maskCep: (cep: number | string) => string;
 
-declare module 'lib-masks' {
-  export function maskNumber (input: number | string): string
-  export function maskDate (input: number | string): string
-  export function maskCurrency (input: number | string): string
-  export function maskCpf (input: number | string): string
-  export function maskCnpj (input: number | string): string
-  export function maskPhone (input: number | string): string
-  export function maskCep (input: number | string): string
-}
+export { maskCep, maskCnpj, maskCpf, maskCurrency, maskDate, maskNumber, maskPhone };
